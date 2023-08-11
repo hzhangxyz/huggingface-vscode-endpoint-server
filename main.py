b96d51bba80b2f2f8dfc5d956e2488defea941a9
Hao Zhang <hzhangxyz@outlook.com>
2023-08-12 01:43:05 +0800

    Add compatibility for CodeGeeX

--- main.py
+++ main.py
@@ -7,9 +7,7 @@ import json
 from util import logger, get_parser
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware
-)
+app.add_middleware(CORSMiddleware)
 generator: GeneratorBase = ...
 
 
@@ -23,7 +21,34 @@ async def api(request: Request):
     logger.info(f'{request.client.host}:{request.client.port} generated_text = {json.dumps(generated_text)}')
     return {
         "generated_text": generated_text,
-        "status": 200
+        "status": 200,
+    }
+
+
+# Add compatibility for CodeGeeX vscode extension
+@app.post("/api/v2/multilingual_code_generate")
+@app.post("/api/v2/multilingual_code_generate_adapt")
+@app.post("/api/v2/multilingual_code_generate_block")
+async def api(request: Request):
+    json_request: dict = await request.json()
+    prefix: str = json_request.pop("prompt")
+    suffix: str = json_request.pop("suffix")
+    prompt: str = prefix if suffix == "" else f"<fim_prefix>{prefix}<fim_suffix>{suffix}<fim_middle>"
+    json_request["max_new_tokens"] = 32
+    logger.info(f'{request.client.host}:{request.client.port} inputs = {json.dumps(prompt)}')
+    generated_text: str = generator.generate(prompt, json_request)
+    logger.info(f'{request.client.host}:{request.client.port} generated_text = {json.dumps(generated_text)}')
+    return {
+        "status": 0,
+        "message": "success",
+        "result": {
+            "app": "multilingual_code_generate",
+            "input": json_request,
+            "output": {
+                "errcode": 0,
+                "code": [generated_text[len(prompt):]],
+            },
+        },
     }
 
 
